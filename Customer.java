ac41f8e3f873528567b36fda02453c80a53b5d4f
FrankJunkar <70077539+FrankJunkar@users.noreply.github.com>
2023-05-25 07:26:04 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Customer {
+    // Customer has name, email address, number of purchases, and a bag for shopping for items
     private String name;
     private String email;
     private int purchases = 0;
     private ArrayList<String> bag = new ArrayList<String>();
-
+    
+    /**
+    * Constructor for Customer object
+    * @param name, email
+    * @author FrankJunkar
+    */
     public Customer(String name, String email){
         this.name = name;
         this.email = email;
@@ -17,6 +23,7 @@ public class Customer {
      * The function will go through a store's inventory and print it out to the user. If the user inputs a valid number
      * the item will be added to their bag, and their total purchases will be increased.
      * @param shoppingStore
+     * @author FrankJunkar
      */
     public void purchaseItem(Store shoppingStore){
         int number = 0;
@@ -25,6 +32,8 @@ public class Customer {
         for(int i = 0; i < inv.size(); i++ ){
             System.out.println(Integer.toString(i+1) + ": " + inv.get(i));
         }
+        
+        // Keeps putting new items for purchase into bag
         while(true){
             number = 0;
             System.out.print("Enter the number for the item that you would like to purchase: ");
@@ -41,6 +50,7 @@ public class Customer {
     /**
      * Return all the items in a customer's bag
      * @return
+     * @author FrankJunkar
      */
     public ArrayList<String> getBagItems(){
         return bag;
@@ -49,6 +59,7 @@ public class Customer {
     /**
      * Returns the amount of purchases that a customer has made
      * @return
+     * @author FrankJunkar
      */
     public int getPurchases(){
         return purchases;
@@ -57,6 +68,7 @@ public class Customer {
     /**
      * Will print the customer's class in a more user readable way
      * @return
+     * @author FrankJunkar
      */
     @Override
     public String toString() {
